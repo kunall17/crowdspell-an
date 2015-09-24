bbdb68ad34157c2ae8b224345e3ff9a8caf3f097
Kunal <kunall.gupta17@gmail.com>
2015-09-24 11:30:55 +0530

    Gradle Added

---
+++ asd - Copy.bat
@@ -0,0 +1,2 @@
+D:\android\sdk\platform-tools\adb.exe install -r -d C:\CrowdWord2\app\build\outputs\apk\app-debug-unaligned.apk
+pause
---
+++ asd.bat
@@ -0,0 +1,7 @@
+call gradlew assembleDebug
+pause 
+pause 
+pause
+pause
+D:\android\sdk\platform-tools install -r -d C:\CrowdWord2\app\build\outputs\apk\app-debug.apk
+pause
